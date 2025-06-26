fa8282f251cd8a38df9e6c2d18d76d469b08d0dc
jeabin5792 <woqls0512@naver.com>
2025-06-26 17:08:54 +0900

    힌트제공 수정

--- src/teamTask/Game.java
+++ src/teamTask/Game.java
@@ -9,7 +9,6 @@ public class Game {
 	int tryCount; // 시도 횟수
 	int limit; // 입력제한수
 	int input;
-	boolean setHintRange = true;
 	int rn1;
 	int rn2;
 
@@ -61,18 +60,13 @@ public class Game {
 			System.out.println("!!힌트!! : 정답보다 작은수를 입력했습니다!!");
 		}
 
-//		      if (this.setHintRange) {
-//		         this.rn1 = (int) (Math.random() * 10); // 3 5
-//		         this.rn2 = (int) (Math.random() * 10); // 3 5
-//		         this.setHintRange = false;
-//		      }
 
 		if (this.tryCount == this.limit / 2) {
-			this.rn1 = (int) (Math.random() * 10); // 3 5
-			this.rn2 = (int) (Math.random() * 10); // 3 5
+			this.rn1 = (int) (Math.random() * 10); 
+			this.rn2 = (int) (Math.random() * 10); 
 		} else if (this.tryCount == this.limit - 2) {
-			this.rn1 = (int) (Math.random() * 3); // 3 5
-			this.rn2 = (int) (Math.random() * 3); // 3 5
+			this.rn1 = (int) (Math.random() * 3); 
+			this.rn2 = (int) (Math.random() * 3); 
 		}
 
 		// 힌트 범위가 정답 범위를 벗어나면 (범위와 정답의 차이만큼만)으로 조정하기
@@ -83,9 +77,6 @@ public class Game {
 			this.rn2 = this.level * 20 - this.answer;
 		}
 
-//		      if (this.tryCount == this.limit - 1) {
-//		         this.setHintRange = true;
-//		      }
 
 		System.out.printf("정답은 %d에서 %d사이에 있습니다.\n", this.answer - this.rn1, this.answer + this.rn2);
 
@@ -104,11 +95,13 @@ public class Game {
 			if (input < 1 || input > (this.level * 20)) {
 				System.out.println("!!!!!!!!범위를 벗어난 입력입니다.!!!!!!!!!");
 			} else {
-				System.out.println(tryCount + "번 입력을 했습니다!");
+				System.out.println(tryCount+1 + "번 입력을 했습니다!");
 				if (checkAnswer()) {
 					break;
 				}
 				if (this.limit / 2 == tryCount) {
+					getHint();
+				}else if(this.tryCount == this.limit-2){
 					getHint();
 				}
 			}
